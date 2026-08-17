6043f297c01ac18d3f8d5937dbb66e268352bad3
k3rasjan <mateusz.najsarek@gmail.com>
2026-08-17 10:54:05 +0200

    add parsers for treesitter to install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,21 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      -- The new API uses the plain 'nvim-treesitter' module name
       local config = require("nvim-treesitter")
       config.setup()
-      vim.api.nvim_create_autocmd('FileType', { callback = function(args) pcall(vim.treesitter.start, args.buf) end,
+      config.install({
+        "lua",
+        "svelte",
+        "html",
+        "html_tags",
+        "javascript",
+        "typescript",
+        "python"
+      })
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
       })
     end,
   },
